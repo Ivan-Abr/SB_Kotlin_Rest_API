2e5a472a9471cb6c8751ede8c466bed746878f00
ivabr <ivabramov16@gmail.com>
2023-10-15 18:17:22 +0300

    Integrational getAllBooks + getBookById + postBook + updateBook + assign tests

--- src/test/kotlin/com/example/demo_kt/integral/BookIntegralControllerTest.kt
+++ src/test/kotlin/com/example/demo_kt/integral/BookIntegralControllerTest.kt
@@ -1,19 +1,18 @@
 package com.example.demo_kt.integral
 
 import com.example.demo_kt.DemoKtApplication
+import com.example.demo_kt.models.Author
 import com.example.demo_kt.models.Book
+import com.example.demo_kt.repositories.AuthorRepository
 import com.example.demo_kt.repositories.BookRepository
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-//import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.TestPropertySource
-import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -26,15 +25,20 @@ import java.time.LocalDate
 @TestPropertySource(locations = ["classpath:application-test.properties"])
 class IntegralTest( @Autowired private val mvc: MockMvc,
                     @Autowired private val bookRepository: BookRepository,
+                    @Autowired private val authorRepository: AuthorRepository,
                     @Autowired private val objectMapper: ObjectMapper) {
 
     lateinit var book: Book
     lateinit var book2: Book
+    lateinit var author: Author
+
     @BeforeEach
     fun initAndSave(){
         book = Book(1L,null, "fst_test","Spring", LocalDate.now(), "normal")
         book2 = Book(2L,null,"sec_test","Spring", LocalDate.now(), "good")
+        author = Author(1L, null,"Misha","Mish@mail.ru", LocalDate.now(), 23)
         bookRepository.save(book)
+        authorRepository.save(author)
     }
     @Test
     @Throws(java.lang.Exception::class)
@@ -69,7 +73,15 @@ class IntegralTest( @Autowired private val mvc: MockMvc,
         mvc.perform(MockMvcRequestBuilders
             .put("/api/v1/book/1?name=testName&annotation=testAnnot")
             .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    @Throws(java.lang.Exception::class)
+    fun assignAuthorToBook(){
+        mvc.perform(MockMvcRequestBuilders.put("/api/v1/book/1/author/1")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
     }
 
 
